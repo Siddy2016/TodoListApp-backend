f4ff72f3103487c345b25d7724c1e2d7998b6934
SIDDHARTH SINGH <musicsiddharth@gmail.com>
2019-03-30 12:45:56 +0530

    Update appConfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,7 +1,7 @@
 let appConfiguration = {
- port : 3000,
+ port : 3004,
  env:"dev",
- db:{uri:"mongodb://127.0.0.1:27017/TodoListAppDB"},
+ db:{uri:"mongodb://127.0.0.1:27017/todo"},
  apiVersion:"/api/v1.0",
  allowedCorsOrigin:"*",
 };
