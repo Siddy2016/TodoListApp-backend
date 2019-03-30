f05e110c53d824ff88984e30bdcfc617ce16370d
SIDDHARTH SINGH <musicsiddharth@gmail.com>
2019-03-30 10:40:33 +0530

    Add files via upload

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "TodoList Application",
+  "version": "0.0.1",
+  "description": "Documentation for the todolist app",
+  "title": "TODOLIST APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-25T10:03:31.407Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
